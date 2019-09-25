b4c87d230f03d5f23230551fb2360b1b7b9a749d
anton-shikov <36560928+anton-shikov@users.noreply.github.com>
2019-09-26 01:05:51 +0300

    Update Manhattan_plot_viz.r

--- Manhattan_plot_viz.r
+++ Manhattan_plot_viz.r
@@ -1,14 +1,10 @@
-library(qqman)
 library(dplyr)
 library(ggplot2)
+require(grid)
+
 manhattan_file = read.csv('herit_Clustered_mahattan_pval_jaccard.tsv', header = T, sep = "\t") 
-#signif_names = read.csv('herit_valuable_SNP_jaccard.tsv', header = FALSE, sep = "\t")
-#str(manhattan_file)
+colnames(manhattan_file) = c('CHR', "BP", "SNP", "Assos_list", "Assos_num")
 
-#manhattan_file[which(manhattan_file$V3 %in% signif_names$V1),] %>% write.table(file = "herit_valuable_SNP_jaccard_for_func", quote = FALSE, sep = "\t", col.names = FALSE, row.names = FALSE)
-
-#manhattan(manhattan_file, chr="V1", bp="V2", snp="V3", p="V5", logp = FALSE)
-colnames(manhattan_file) = c('CHR', "BP", "SNP", "Assos_list", "Assos_num", 'logp')
 
 don <- manhattan_file %>% 
   group_by(CHR) %>% 
@@ -21,8 +17,8 @@ don <- manhattan_file %>%
 axisdf = don %>% group_by(CHR) %>% summarize(center=( max(BPcum) + min(BPcum) ) / 2 )
 
 
-#require(grid)
-#dev.new()
+
+dev.new()
 pdf("_pval_clust_manhattan_2.pdf")
 ggplot(don, aes(x=BPcum, y=-log10(logp))) +
   geom_point( aes(color=as.factor(CHR)), alpha=0, size=6) +
@@ -43,37 +39,7 @@ dev.off()
 
 
 
-ggplot(don, aes(x=BPcum, y=-log10(logp))) +
-  geom_point( aes(color=as.factor(CHR)), alpha=0.9, size=2.8) +
-  scale_color_manual(values = rep(c("#808B96", "#A4EAE8"), 22)) +
-  scale_x_continuous(label = axisdf$CHR, breaks= axisdf$center ) +
-  scale_y_continuous(expand = c(0, 0),limits = c(0, 35)) +
-  coord_fixed((1 * max(don$BPcum) / (max(-log10(don$logp))))/2.5)+
-  xlab('Genomic coordinate') + ylab('Number of associations')+theme_void()+
-  theme( legend.position="none")
-  
+#colnames(manhattan_file) = c('CHR', "BP", "SNP", "Assos_list", "Assos_num", 'logp')
 
 
-
-c("#808B96", "#A4EAE8")
-c("#1DAEC5","#AF77BD")
 manhattan_file %>% select(SNP, Assos_list, Assos_num) %>% write.table(file = "herit_clust_diags_jaccard.tsv", quote = FALSE, sep = "\t", col.names = FALSE, row.names = FALSE)
-
-
-require(grid)
-# Make the plots
-dev.new()  # Reducing width and height of this device will produce smaller raster files
-plot(rnorm(2e4), rnorm(2e4), pch="+", cex=0.6)
-cap1 <- grid.cap()
-plot(rnorm(2e4), rnorm(2e4), pch="+", cex=0.6, col="red")
-cap2 <- grid.cap()
-dev.off()
-
-# Write them to a pdf
-pdf("test.pdf", onefile=TRUE)
-grid.raster(cap1)
-plot.new()
-grid.raster(cap2)
-dev.off()
-
-#c("blue","yellow", "green", "grey", "skyblue", "purple","black","orange", "red", "cyan", "pink"), 2
